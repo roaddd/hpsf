631e7e8f53f33d67eb6c46cf8d8344fa44e66d19
KeLong Huang <15153466575@163.com>
2023-03-14 17:06:43 +0800

    test:io_context_test

--- coroutine/test/io_context_test.cpp
+++ coroutine/test/io_context_test.cpp
@@ -1,25 +1,42 @@
 #include "co/Task.hpp"
 #include "co/io_context.hpp"
-#include "utils.hpp"
+#include "net/Acceptor.hpp"
+#include "Utils.hpp"
+#include "net/Socket.hpp"
 
 #include <coroutine>
 #include <unistd.h>
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 using namespace co;
 
-Task<void> test()
-{
-    sleep(3);
-    debug("run test() after 3s\n");    
-    co_await TaskAwaiter<void>{};
+io_context context;
+
+Task<> session(int sockfd){
+    char buf[8192];
+    int pos_r=0,pos_s=0;
+    Socket sock{&context,sockfd};
+    while(true){
+        int nr=co_await sock.receive(buf,pos_r,8192);
+        co_await sock.send(buf,pos_s,pos_r);
+    }
+}
+
+Task<> server(io_context* co){
+    debug("run work()\n");
+    Acceptor accept(co);
+    accept.start();
+    for(int sock;sock=co_await accept.accept();){
+        //debug("new connextion come: fd= ",sock);
+        context.co_spawn(session(sock),sock);
+    }
 } 
 
 int main()
 {
-    io_context context;
-    context.co_spawn(test());
-    context.run();
+    context.co_spawn(server(&context),-1);
+    context.start();
     context.join();
 }
